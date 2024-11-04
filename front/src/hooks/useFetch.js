fc8a33005e56433dbf3fbf97c3e832377ba4b902
Damien <damien.dev2022@gmail.com>
2024-11-04 14:32:03 +0100

    correction pour passer des données mocker a l'api

--- front/src/hooks/useFetch.js
+++ front/src/hooks/useFetch.js
@@ -4,7 +4,7 @@ import dataMocked from "../data.json";
 const URL_API = "http://localhost:3000";
 
 // permet d'utiliser les données mocké
-const MOCKED = true;
+const MOCKED = false;
 
 console.info(
   MOCKED ? "Les données sont mocker" : "Les données viennent du service"
@@ -25,20 +25,19 @@ const useFetch = (userId, endpoint, defaultData) => {
 
     setIsLoading(true);
     fetch(`${URL_API}/user/${userId}/${endpoint}`)
-      .then(async (response) => {
-        console.log(response);
-        const text = await response.text();
-        console.log(text);
-        return response;
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
       })
-      .then((response) => response.json())
       .then((json) => json?.data)
       .then((data) => {
         setData(data);
         setError(null);
       })
       .catch((error) => {
-        setError("Echec de chargement des données");
+        setError("Échec de chargement des données");
         console.error(error);
       })
       .finally(() => setIsLoading(false));
